9848eeef66113c49c4337385992e3c7909360926
NIRMESH <nirmesh.mashru@gmail.com>
2017-10-22 18:57:52 +0530

    add mime type

--- resources/views/index2.blade.php
+++ resources/views/index2.blade.php
@@ -19,7 +19,7 @@
     <!--rel="stylesheet"-->
     <!--href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">-->
 
-    <link rel="stylesheet" href="{{asset("v2/assets/css/styles.min.css")}}"/>
+    <link rel="stylesheet" type="text/css" href="{{asset("v2/assets/css/styles.min.css")}}"/>
 
 </head>
 <body>
